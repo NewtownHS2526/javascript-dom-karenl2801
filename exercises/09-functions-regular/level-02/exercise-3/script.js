a6b2d9570ce20bcd6eaaa81219de45e701cd0e04
Karen Lorin <karenl2801@nycstudents.net>
2025-11-30 11:52:03 -0500

    09 done

--- exercises/09-functions-regular/level-02/exercise-3/script.js
+++ exercises/09-functions-regular/level-02/exercise-3/script.js
@@ -6,3 +6,13 @@
 //     return "Adult";
 // }
 
+function checkAge(age) {
+    if (age < 0) {
+        return "Invalid age";
+    }
+    if (age < 18) {
+        return "Minor";
+    }
+    return "Adult";
+}
+console.log(checkAge(20));
