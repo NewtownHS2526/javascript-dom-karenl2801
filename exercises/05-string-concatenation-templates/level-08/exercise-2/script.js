6ad6a8ee5fab65e5cce7b3c9e4a189ed700408ce
Karen Lorin <karenl2801@nycstudents.net>
2025-11-30 11:21:25 -0500

    05 done

--- exercises/05-string-concatenation-templates/level-08/exercise-2/script.js
+++ exercises/05-string-concatenation-templates/level-08/exercise-2/script.js
@@ -1,3 +1,6 @@
 // TODO: Create a sentence by concatenating multiple strings
 // Example: "Hello" + ", " + name + "! Welcome!"
-
+let greeting = "Hello";
+let name = "Alina";
+let message = greeting + ", " + name + "! Welcome!";
+console.log(message);
