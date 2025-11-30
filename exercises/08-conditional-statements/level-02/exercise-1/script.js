bc0533bb5216d5b1dd107f6efb5ffb0dd3e9d7e7
Karen Lorin <karenl2801@nycstudents.net>
2025-11-30 11:45:33 -0500

    08 done

--- exercises/08-conditional-statements/level-02/exercise-1/script.js
+++ exercises/08-conditional-statements/level-02/exercise-1/script.js
@@ -3,4 +3,8 @@
 // if (username === "Miles" && age === 18) {
 //     console.log("Welcome");
 // }
-
+let username = "Miles";
+let age = 18;
+if (username === "Miles" && age === 18) {
+    console.log("Welcome, Spiderman");
+}
